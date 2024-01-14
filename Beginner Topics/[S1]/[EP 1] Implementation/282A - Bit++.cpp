02e45d48c8dd4710ad0ceda38345a9210bf26f2b
Shihab <mdshihabhossain1512@gmail.com>
2024-01-14 10:59:20 +0600

    Create 282A - Bit++.cpp

---
+++ Beginner Topics/[S1]/[EP 1] Implementation/282A - Bit++.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    int x = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+
+        if (s[1] == '+')
+        {
+            x++;
+        }
+        else if (s[1] == '-')
+        {
+            x--;
+        }
+    }
+    cout << x << "\n";
+}
